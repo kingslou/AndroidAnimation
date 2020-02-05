339bbac11dc0724555f704f3a934c7c40ff3c4ab
geen <loujin_007@126.com>
2020-02-05 16:25:57 +0800

    first init

---
+++ app/src/main/java/com/example/androidanimation/AnimationView.java
@@ -0,0 +1,180 @@
+package com.example.androidanimation;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.TypeEvaluator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+/**
+ * 商品动画自定义View
+ * Create by: chenwei.li
+ * Date: 2017/4/23
+ * Time: 下午9:21
+ * Email: lichenwei.me@foxmail.com
+ */
+
+public class AnimationView extends View {
+
+    //小红点开始坐标
+    Point mCircleStartPoint = new Point();
+    //小红点结束坐标
+    Point mCircleEndPoint = new Point();
+    //小红点控制点坐标
+    Point mCircleConPoint = new Point();
+    //小红点的移动坐标
+    Point mCircleMovePoint = new Point();
+    //小红点半径
+    private int mRadius = 20;
+    //小红点画笔
+    private Paint mCirclePaint;
+
+    public AnimationView(Context context) {
+        super(context);
+        init(context);
+    }
+
+
+    public AnimationView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public AnimationView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawCircle(canvas);
+    }
+
+    /**
+     * 进行一些初始化操作
+     */
+    private void init(Context context) {
+        mCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mCirclePaint.setStyle(Paint.Style.FILL);
+        mCirclePaint.setColor(Color.RED);
+
+
+    }
+
+    /**
+     * 商品加入购物车的小红点
+     */
+    private void drawCircle(Canvas canvas) {
+        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.mipmap.one), mCircleMovePoint.x, mCircleMovePoint.y, mCirclePaint);
+    }
+
+    /**
+     * 设置开始点和移动点
+     *
+     * @param x
+     * @param y
+     */
+    public void setCircleStartPoint(int x, int y) {
+        this.mCircleStartPoint.x = x;
+        this.mCircleStartPoint.y = y;
+        this.mCircleMovePoint.x = x;
+        this.mCircleMovePoint.y = y;
+    }
+
+    public void setImageResource() {
+
+    }
+
+    /**
+     * 设置结束点
+     *
+     * @param x
+     * @param y
+     */
+    public void setCircleEndPoint(int x, int y) {
+        this.mCircleEndPoint.x = x;
+        this.mCircleEndPoint.y = y;
+    }
+
+
+    /**
+     * 开始动画
+     */
+    public void startAnimation(final KissAnimationListener listener) {
+        if (mCircleStartPoint == null || mCircleEndPoint == null) {
+            return;
+        }
+
+
+        //设置控制点
+        mCircleConPoint.x = ((mCircleStartPoint.x + mCircleEndPoint.x) / 2);
+        mCircleConPoint.y = (20);
+
+        //设置值动画
+        ValueAnimator valueAnimator = ValueAnimator.ofObject(new CirclePointEvaluator(), mCircleStartPoint, mCircleEndPoint);
+        valueAnimator.setDuration(600);
+        valueAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                Point goodsViewPoint = (Point) animation.getAnimatedValue();
+                mCircleMovePoint.x = goodsViewPoint.x;
+                mCircleMovePoint.y = goodsViewPoint.y;
+                invalidate();
+            }
+        });
+        valueAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                ViewGroup viewGroup = (ViewGroup) getParent();
+                viewGroup.removeView(AnimationView.this);
+                listener.end();
+            }
+        });
+        valueAnimator.start();
+
+    }
+
+    public interface KissAnimationListener{
+
+        void end();
+    }
+
+
+    /**
+     * 自定义Evaluator
+     */
+    public class CirclePointEvaluator implements TypeEvaluator {
+
+        /**
+         * @param t          当前动画进度
+         * @param startValue 开始值
+         * @param endValue   结束值
+         * @return
+         */
+        @Override
+        public Object evaluate(float t, Object startValue, Object endValue) {
+
+            Point startPoint = (Point) startValue;
+            Point endPoint = (Point) endValue;
+
+            int x = (int) (Math.pow((1 - t), 2) * startPoint.x + 2 * (1 - t) * t * mCircleConPoint.x + Math.pow(t, 2) * endPoint.x);
+            int y = (int) (Math.pow((1 - t), 2) * startPoint.y + 2 * (1 - t) * t * mCircleConPoint.y + Math.pow(t, 2) * endPoint.y);
+
+            return new Point(x, y);
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/androidanimation/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.androidanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageAirShip, imageOne, imageTwo, imageThree, imageSelect;
+    private int mShipWidth;
+    private ViewGroup mViewGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageAirShip = findViewById(R.id.imageShip);
+        imageOne = findViewById(R.id.imageOne);
+        imageTwo = findViewById(R.id.imageTwo);
+        imageThree = findViewById(R.id.imageThree);
+        imageSelect = findViewById(R.id.imageSelect);
+        mViewGroup = (ViewGroup) getWindow().getDecorView();
+
+        imageOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //获取商品坐标
+                int[] goodsPoint = new int[2];
+                imageOne.getLocationInWindow(goodsPoint);
+
+                //获取购物车坐标
+                int[] shoppingCartPoint = new int[2];
+                imageAirShip.getLocationInWindow(shoppingCartPoint);
+
+                //生成商品View
+                AnimationView animationView = new AnimationView(MainActivity.this);
+                animationView.setCircleStartPoint(goodsPoint[0], goodsPoint[1]);
+                animationView.setCircleEndPoint(shoppingCartPoint[0] + mShipWidth / 2, shoppingCartPoint[1]);
+                //添加View并执行动画
+                mViewGroup.addView(animationView);
+                animationView.startAnimation(new AnimationView.KissAnimationListener() {
+                    @Override
+                    public void end() {
+                        imageSelect.setVisibility(View.VISIBLE);
+                        imageSelect.setImageResource(R.mipmap.one);
+                    }
+                });
+            }
+        });
+
+        imageTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int[] goodsPoint = new int[2];
+                imageTwo.getLocationInWindow(goodsPoint);
+
+                //获取购物车坐标
+                int[] shoppingCartPoint = new int[2];
+                imageAirShip.getLocationInWindow(shoppingCartPoint);
+
+                //生成商品View
+                AnimationView animationView = new AnimationView(MainActivity.this);
+                animationView.setCircleStartPoint(goodsPoint[0], goodsPoint[1]);
+                animationView.setCircleEndPoint(shoppingCartPoint[0] + mShipWidth / 2, shoppingCartPoint[1]);
+                //添加View并执行动画
+                mViewGroup.addView(animationView);
+                animationView.startAnimation(new AnimationView.KissAnimationListener() {
+                    @Override
+                    public void end() {
+                        imageSelect.setVisibility(View.VISIBLE);
+                        imageSelect.setImageResource(R.mipmap.two);
+                    }
+                });
+            }
+        });
+
+        imageThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int[] goodsPoint = new int[2];
+                imageThree.getLocationInWindow(goodsPoint);
+
+                //获取购物车坐标
+                int[] shoppingCartPoint = new int[2];
+                imageAirShip.getLocationInWindow(shoppingCartPoint);
+
+                //生成商品View
+                AnimationView animationView = new AnimationView(MainActivity.this);
+                animationView.setCircleStartPoint(goodsPoint[0], goodsPoint[1]);
+                animationView.setCircleEndPoint(shoppingCartPoint[0] + mShipWidth / 2, shoppingCartPoint[1]);
+                //添加View并执行动画
+                mViewGroup.addView(animationView);
+                animationView.startAnimation(new AnimationView.KissAnimationListener() {
+                    @Override
+                    public void end() {
+                        imageSelect.setVisibility(View.VISIBLE);
+                        imageSelect.setImageResource(R.mipmap.three);
+                    }
+                });
+            }
+        });
+
+        ViewTreeObserver viewTreeObserver = imageAirShip.getViewTreeObserver();
+        viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                imageAirShip.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                mShipWidth = imageAirShip.getMeasuredWidth();
+            }
+        });
+    }
+
+    private void AddCartAnimation() {
+
+
+    }
+
+}
