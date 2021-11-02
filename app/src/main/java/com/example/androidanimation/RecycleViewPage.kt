8b2b7c0140f63a7bf71012a250bf06a9247f391a
geen <loujin_007@126.com>
2021-11-02 10:08:49 +0800

    Add recycleView指示器

--- app/src/main/java/com/example/androidanimation/RecycleViewPage.kt
+++ app/src/main/java/com/example/androidanimation/RecycleViewPage.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidanimation.utils.StatusBarUtils
 import kotlinx.android.synthetic.main.attivity_recycle_page.*
+import java.util.*
 
 /**
  * @Author LuoJi
@@ -16,10 +19,22 @@ import kotlinx.android.synthetic.main.attivity_recycle_page.*
  */
 class RecycleViewPage : AppCompatActivity() {
 
+    private val intArray :MutableList<Int> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.attivity_recycle_page)
+
+        for(index in 1..20){
+            intArray.add(index)
+        }
+
         val mAdapter = MAdapter()
+
+        val layoutManger = GridLayoutManager(this,2,RecyclerView.HORIZONTAL,false)
+
+        recyclerView.layoutManager = layoutManger
+
         recyclerView.adapter = mAdapter
         indicator.bindRecyclerView(recyclerView)
         StatusBarUtils.setDeepStatusBar(true, this)
@@ -36,12 +51,19 @@ class RecycleViewPage : AppCompatActivity() {
                 )
 
 
-        override fun getItemCount(): Int = 15
+        override fun getItemCount(): Int = intArray.size
 
         override fun onBindViewHolder(holder: MViewHolder, position: Int) {
+
+            holder.tvText?.text = intArray[position].toString()+"什么鬼"
+
+
         }
 
     }
 
-    private inner class MViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    private inner class MViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        var tvText : TextView? =  findViewById(R.id.tvText)
+
+    }
 }
